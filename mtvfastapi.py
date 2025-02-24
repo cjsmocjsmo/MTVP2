1d55eb7702242aa8cca9f290c68a87fbb89790c2
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2025-02-24 04:19:37 -0800

    add FastAPI application with dotenv configuration and update server port

---
+++ mtvfastapi.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from dotenv import load_dotenv
+import os
+import mtvserverutils
+import uvicorn
+
+MTVMEDIA = mtvserverutils.MTVMedia()
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "FastApi Hello, World!"}
+
+@app.get("/action")
+def action():
+    return {MTVMEDIA.action()}
+
+if __name__ == "__main__":
+    
+    load_dotenv()
+
+    host = os.getenv("MTV_RAW_ADDR")
+    port = os.getenv("MTV_SERVER_PORT")
+    
+    uvicorn.run(app, host=host, port=port)
