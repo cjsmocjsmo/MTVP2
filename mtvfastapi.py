c249d977f969a373ff9d21a97e91b9157081aff6
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2025-02-24 06:56:42 -0800

    fix action function to correctly unpack movie data fields

--- mtvfastapi.py
+++ mtvfastapi.py
@@ -80,7 +80,7 @@ def action():
     data = cursor.fetchall()
     print(data)
     conn.close()
-    return [{"Name": Name, "Year": Year, "PosterAddr": data.PosterAddr, "Size": data.Size, "Path": data.Path, "Idx": data.Idx, "MovId": data.MovId, "Catagory": data.Catagory, "HttpThumbPath": data.HttpThumbPath} for Name, Year, PosterAddr, Size, Path, Idx, MovId, Catagory, HttpThumbPath in data]
+    return [{"Name": Name, "Year": Year, "PosterAddr": PosterAddr, "Size": Size, "Path": Path, "Idx": Idx, "MovId": MovId, "Catagory": Catagory, "HttpThumbPath": HttpThumbPath} for Name, Year, PosterAddr, Size, Path, Idx, MovId, Catagory, HttpThumbPath in data]
 
     
 
